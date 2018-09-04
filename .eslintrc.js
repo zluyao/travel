9a0528c8aeea9850abee6bbf6f9fe683912c2a26
Yuhoo <yuhoo@muchenglin.com>
2018-09-04 13:42:30 +0800

    1、设置Eslint 2、引入icon文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@
 
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+    sourceType: 'module'
   },
   env: {
     browser: true,
@@ -11,16 +12,27 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    // 'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'html'
   ],
   // add your custom rules here
   rules: {
+    'comma-dangle': 0,
+    'no-extra-semi': 0,
+    'new-cap': 0,
+    'no-new': 0,
+    'no-undef': 0,
+    'max-len': 0,
+    'no-useless-escape': 0,
+    'space-before-function-paren': [0, 'always'],//函数定义时括号前面要不要有空格
+    'semi': [2, 'always'],//语句强制分号结尾
+    'no-console': 0,// 禁止使用console
+    'no-unused-expressions': 'off',
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
